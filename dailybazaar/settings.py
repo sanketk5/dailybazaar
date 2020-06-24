ad90bbdb8ce5e8129e91a66cc297a676a767ae6f
sanketk5 <43268082+sanketk5@users.noreply.github.com>
2020-06-24 17:48:57 +0530

    Update settings.py

--- dailybazaar/settings.py
+++ dailybazaar/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '68+y553l)4i&ne^!&251i9zph(k!(-f@hejos_n(0(g7@0p+os'
+SECRET_KEY = 'secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
